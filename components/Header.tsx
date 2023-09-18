05653cd4280314496c9416b9b01f1332ac555847
dev2hearts <js.park119de@gmail.com>
2023-09-18 10:47:41 +0900

    feat: header 1차 작업

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,18 @@
+import styels from "@/styles/header.module.scss"
+import Image from "next/image";
+import Link from "next/link";
+
+
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className={styels.header}>
+      <div className={styels.flexitem}>
+        <Link href="/" className={styels.logo}>
+          <Image src="/logo.png" alt="건강증진센터" width={40} height={40}/>
+        </Link>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
