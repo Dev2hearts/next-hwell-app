85dc23a1c96e49fb84a8325a5c4229b77333519c
dev2hearts <js.park119de@gmail.com>
2023-09-20 12:40:24 +0900

    feat: GA 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,33 @@ import '@/styles/globals.scss';
 import { DefaultSeo } from 'next-seo';
 import type { AppProps } from 'next/app';
 import SEO from '@/next-seo.config';
+import Head from 'next/head';
+import * as gtag from '../lib/gtag';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
+       {/* Global Site Tag (gtag.js) - Google Analytics */}
+       <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </>
